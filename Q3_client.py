c8ac74676b43c49cfd965b01345a11e4ba9133da
Arlene <122369943+Arlenepham@users.noreply.github.com>
2024-02-27 21:53:12 -0500

    Update Q3_client.py

--- Q3_client.py
+++ Q3_client.py
@@ -1,1 +1,29 @@
+import socket
+import pickle
+import threading
 
+def receive_messages(client_socket):
+    while True:
+        try:
+            pickled_message = client_socket.recv(1024)
+            if not pickled_message:
+                break
+
+            message = pickle.loads(pickled_message)
+            print(f"Received: {message}")
+            
+        except (ConnectionError, EOFError):
+            print("Error")
+            break
+        
+if __name__ == "__main__":
+        server_address = ("127.0.0.1", 3030)
+        client_socket=  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(server_address)
+
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+        receive_thread.start()
+
+        while True:
+            user_input = input("Enter your message: ")
+            client_socket.sendall(pickle.dumps(user_input))
